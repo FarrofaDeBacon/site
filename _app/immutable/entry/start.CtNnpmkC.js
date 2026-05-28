a09848697f3af09fcf37547db9a018b76962b987
TraderLog Pro <contato@traderlog.pro>
2026-05-28 14:57:51 -0300

    Deploy SvelteKit build to Hostinger

--- _app/immutable/entry/start.CtNnpmkC.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/K3YN9CCt.js";import{m as t}from"../chunks/C6rywRm9.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.D1u83q0j.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/CfNUdJGd.js";import{m as t}from"../chunks/fZ7XBnKs.js";export{t as load_css,r as start};
